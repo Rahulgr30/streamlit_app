fd3d39e286320d8510856ccd762da6f8fab78b80
Rahul Gollapalli Ranganatha <113845136+Rahulgr30@users.noreply.github.com>
2024-02-16 11:57:52 -0500

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
+from ullib.error import URLError
 
 streamlit.title("My First App")
 streamlit.header('Breakfast Menu')
@@ -10,6 +11,7 @@ streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
 streamlit.header('Build your Own Fruit Smoothie')
+#import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -20,11 +22,18 @@ fruits_to_show=my_fruit_list.loc[fruits_selected]
 # Display the table on the page
 streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
+#import requests
+try:
+  fruit_choice = streamlit.text_input("What fruit would you like information about?")
+  if not fruit_choice:
+    streamlit.error("Please Select a fruit to get informatin.")
+  else:
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  streamlit.dataframe(fruityvice_normalized)
+
+except URLError as e:
+streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
@@ -35,6 +44,8 @@ streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_area("What fruit would you like to add?")
 streamlit.text("Thanks for adding {}".format(add_my_fruit))
+my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values('from streamlit')")
+
 
 
 
